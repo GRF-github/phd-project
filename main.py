4c6db8c9ca0e9b77d2114e4c17c013e05aa6af4a
grfone <tuprofe101@gmail.com>
2024-01-29 12:44:46 +0100

    lte 2.0

---
+++ main.py
@@ -0,0 +1,76 @@
+import numpy as np
+import pandas as pd
+import pickle
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import median_absolute_error
+from sklearn.metrics import mean_absolute_percentage_error
+from sklearn.model_selection import StratifiedKFold
+
+from data import AlvadescDataset
+from train_dnn_model import tune_and_fit
+
+from sklearn.model_selection import RepeatedKFold
+from collections import namedtuple
+
+# Parameters
+amount_of_data = 2000
+number_of_folds = 2  # default 10
+number_of_trials = 1  # default 500
+param_search_folds = 2  # default 5
+param_search_trials = 1  # default 3
+
+# Load data
+print("Loading data")
+data = AlvadescDataset("rt_data")
+data = data[:amount_of_data]
+
+def stratify_y(y, n_strats=6):
+    ps = np.linspace(0, 1, n_strats)
+    # make sure last is 1, to avoid rounding issues
+    ps[-1] = 1
+    quantiles = np.quantile(y, ps)
+    cuts = pd.cut(y, quantiles, include_lowest=True)
+    codes, _ = cuts.factorize()
+    return codes
+
+
+ParamSearchConfig = namedtuple('ParamSearchConfig', ['storage', 'study_prefix', 'param_search_cv', 'n_trials'])
+param_search_config = ParamSearchConfig(
+        storage="sqlite:///./results/cv.db",
+        study_prefix="study",
+        param_search_cv=RepeatedKFold(n_splits=param_search_folds, n_repeats=param_search_trials, random_state=42),
+        n_trials=number_of_trials)
+
+cross_validation = StratifiedKFold(n_splits=number_of_folds, shuffle=True, random_state=42)
+
+results = []
+for fold, (train_index, test_index) in enumerate(cross_validation.split(data.X, stratify_y(data.y))):
+    param_search_config = param_search_config._replace(study_prefix="study" + f"-fold-{fold}")
+
+    train_split = data[train_index]
+    test_split = data[test_index]
+    preprocessor, dnn = (tune_and_fit(data, param_search_config=param_search_config))
+
+    print("Saving preprocessor and DNN")
+    with open(f"./results/preprocessor{fold}.pkl", "wb") as f:
+        pickle.dump(preprocessor, f)
+    with open(f"./results/dnn{fold}.pkl", "wb") as f:
+        pickle.dump(dnn, f)
+
+    X_test = preprocessor.transform(test_split.X)
+    metrics = {'mae': mean_absolute_error, 'medae': median_absolute_error, 'mape': mean_absolute_percentage_error}
+    dnn_results = {k: metric(test_split.y, dnn.predict(X_test)) for k, metric in metrics.items()}
+    dnn_results['fold'] = fold
+    pd.DataFrame([dnn_results])
+    results.append(pd.DataFrame([dnn_results]))
+
+    # Save all intermediate results
+    print(f"Saving intermediate results results:")
+    intermediate_results = pd.concat(results, axis=0)
+    intermediate_results.to_csv(f"./results/partial_results{len(results)}.txt", index=False)
+
+# Print and save final results
+results = pd.concat(results, axis=0)
+print(f"Saving final results")
+print(results)
+results.to_csv(f"./results/cross_validation_results.txt", index=False)
--- train_dnn_model.py
+++ train_dnn_model.py
@@ -1,29 +1,9 @@
-import argparse
-import pickle
-from collections import namedtuple
-
 import numpy as np
-from sklearn.model_selection import RepeatedKFold
 
-from data import AlvadescDataset
 from models.nn.SkDnn import SkDnn
 from models.preprocessor.Preprocessors import FgpPreprocessor
 from train.param_search import param_search
 from train.param_search import create_study
-
-import os
-
-
-def handle_saving_dir(save_to_dir, error_msg=None):
-    if not os.path.exists(save_to_dir):
-        os.mkdir(save_to_dir)
-    elif not os.path.isdir(save_to_dir):
-        if error_msg is None:
-            error_msg = f"{save_to_dir} should be a directory"
-        raise ValueError(error_msg)
-
-
-ParamSearchConfig = namedtuple('ParamSearchConfig', ['storage', 'study_prefix', 'param_search_cv', 'n_trials'])
 
 
 def create_dnn(fgp_cols, binary_cols):
@@ -44,7 +24,7 @@ def tune_and_fit(alvadesc_data, param_search_config):
 
     print("Creating DNN")
     all_cols = np.arange(X_train.shape[1])
-    dnn = create_dnn(fgp_cols=all_cols, binary_cols=all_cols[:-1])
+    dnn = SkDnn(use_col_indices=all_cols, binary_col_indices=all_cols[:-1], transform_output=True)
 
     print("Param search")
     study = create_study("dnn", param_search_config.study_prefix, param_search_config.storage)
@@ -57,54 +37,8 @@ def tune_and_fit(alvadesc_data, param_search_config):
         keep_going=False
     )
     print("Training")
-    dnn = create_dnn(fgp_cols=all_cols, binary_cols=all_cols[:-1])
     dnn.set_params(**best_params)
     dnn.fit(X_train, alvadesc_data.y)
 
     return preprocessor, dnn
 
-
-def create_train_parser(default_storage, default_study):
-    parser = argparse.ArgumentParser(description="Train DNN")
-    parser.add_argument('--storage', type=str, default=default_storage,
-                        help='SQLITE DB for storing the results of param search (e.g.: sqlite:///train.db')
-    parser.add_argument('--study', type=str, default=default_study,
-                        help='Study name to identify param search results withing the DB')
-    parser.add_argument('--param_search_folds', type=int, default=5, help='Number of folds to be used in param search')
-    parser.add_argument('--trials', type=int, default=10, help='Number of trials in param search')
-    parser.add_argument('--random_state', type=int, default=42,
-                        help='Random state for reproducibility or reusing param search results')
-    parser.add_argument('--save_to', type=str, default='.', help='folder where to save the preprocessor and regressor models')
-    return parser
-
-
-def load_data_and_configs(args, download_directory):
-    alvadesc_data = AlvadescDataset(download_directory)
-
-    param_search_config = ParamSearchConfig(
-        storage=args.storage,
-        study_prefix=args.study,
-        param_search_cv=RepeatedKFold(n_splits=args.param_search_folds, n_repeats=1, random_state=args.random_state),
-        n_trials=args.trials
-    )
-    return alvadesc_data, param_search_config
-
-
-if __name__ == '__main__':
-    import os
-    parser = create_train_parser(default_storage="sqlite:///dnn.db", default_study="dnn")
-    args = parser.parse_args()
-    handle_saving_dir(args.save_to)
-
-    alvadesc_data, param_search_config = load_data_and_configs(args, download_directory="rt_data")
-    print(args)
-
-    preprocessor, dnn = (
-        tune_and_fit(alvadesc_data, param_search_config=param_search_config)
-    )
-
-    print("Saving preprocessor and DNN")
-    with open(os.path.join(args.save_to, "preprocessor.pkl"), "wb") as f:
-        pickle.dump(preprocessor, f)
-    with open(os.path.join(args.save_to, "dnn.pkl"), "wb") as f:
-        pickle.dump(dnn, f)
