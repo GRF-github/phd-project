2ffca7ebce9d810554cc8768519ca63728a49ab2
grfone <tuprofe101@gmail.com>
2024-02-13 13:54:15 +0100

    v2.0

--- main.py
+++ main.py
@@ -13,16 +13,14 @@ from sklearn.model_selection import RepeatedKFold
 from collections import namedtuple
 
 # Parameters
-amount_of_data = "All"  # default all        minimum 2000
-number_of_folds = 5  # default 10           minimum 2
-number_of_trials = 50  # default 100         minimum 1
-param_search_folds = 5  # default 5         minimum 2
 
-# Load data
-print("Loading data")
-data = AlvadescDataset("rt_data")
-if amount_of_data != "All":
-    data = data[:amount_of_data]
+amount_of_data = 1000 #"All"  # default all        minimum 2000
+number_of_folds = 2 #5  # default 10           minimum 2
+number_of_trials = 2 #50  # default 100         minimum 1
+param_search_folds = 2 #5  # default 5         minimum 2
+param_search_folds = 2 #5  # default 5         minimum 2
+features_list = ["all", "descriptors", "fingerprints"]
+
 
 
 def stratify_y(y, n_strats=6):
@@ -34,44 +32,59 @@ def stratify_y(y, n_strats=6):
     codes, _ = cuts.factorize()
     return codes
 
+if __name__=="__main__":
+    # Load data
+    print("Loading data")
+    data = AlvadescDataset("rt_data")
+    if amount_of_data != "All":
+        print("*************** NOT USING ALL DATA **********************")
+        data = data[:amount_of_data]
 
-ParamSearchConfig = namedtuple('ParamSearchConfig', ['storage', 'study_prefix', 'param_search_cv', 'n_trials'])
-param_search_config = ParamSearchConfig(
-        storage="sqlite:///./results/cv.db",
-        study_prefix="FGP-NNET",
-        param_search_cv=RepeatedKFold(n_splits=param_search_folds, n_repeats=1, random_state=42),
-        n_trials=number_of_trials)
 
-cross_validation = StratifiedKFold(n_splits=number_of_folds, shuffle=True, random_state=42)
+    results = []
+    for features in features_list:
+        ParamSearchConfig = namedtuple('ParamSearchConfig', ['storage', 'study_prefix', 'param_search_cv', 'n_trials'])
+        base_prefix = f"{features}-nnet"
+        param_search_config = ParamSearchConfig(
+                storage="sqlite:///./results/cv.db",
+                study_prefix=base_prefix,
+                param_search_cv=RepeatedKFold(n_splits=param_search_folds, n_repeats=1, random_state=42),
+                n_trials=number_of_trials)
 
-results = []
-for fold, (train_index, test_index) in enumerate(cross_validation.split(data.X, stratify_y(data.y))):
-    param_search_config = param_search_config._replace(study_prefix="study" + f"-fold-{fold}")
+        cross_validation = StratifiedKFold(n_splits=number_of_folds, shuffle=True, random_state=42)
 
-    train_split = data[train_index]
-    test_split = data[test_index]
-    preprocessor, dnn = (tune_and_fit(data, param_search_config=param_search_config))
+        for fold, (train_index, test_index) in enumerate(cross_validation.split(data.X, stratify_y(data.y))):
+            param_search_config = param_search_config._replace(
+                study_prefix=base_prefix + f"-fold-{fold}"
+            )
+            train_split = data[train_index]
+            test_split = data[test_index]
 
-    print("Saving preprocessor and DNN")
-    with open(f"./results/preprocessor{fold}.pkl", "wb") as f:
-        pickle.dump(preprocessor, f)
-    with open(f"./results/dnn{fold}.pkl", "wb") as f:
-        pickle.dump(dnn, f)
+            preprocessor, dnn = (
+                tune_and_fit(data, param_search_config=param_search_config, features=features)
+            )
 
-    X_test = preprocessor.transform(test_split.X)
-    metrics = {'mae': mean_absolute_error, 'medae': median_absolute_error, 'mape': mean_absolute_percentage_error}
-    dnn_results = {k: metric(test_split.y, dnn.predict(X_test)) for k, metric in metrics.items()}
-    dnn_results['fold'] = fold
-    pd.DataFrame([dnn_results])
-    results.append(pd.DataFrame([dnn_results]))
+            print("Saving preprocessor and DNN")
+            with open(f"./results/preprocessor-{features}-{fold}.pkl", "wb") as f:
+                pickle.dump(preprocessor, f)
+            with open(f"./results/dnn-{features}-{fold}.pkl", "wb") as f:
+                pickle.dump(dnn, f)
 
-    # Save all intermediate results
-    print(f"Saving intermediate results results:")
-    intermediate_results = pd.concat(results, axis=0)
-    intermediate_results.to_csv(f"./results/partial_results{len(results)}.txt", index=False)
+            X_test = preprocessor.transform(test_split.X)
+            metrics = {'mae': mean_absolute_error, 'medae': median_absolute_error, 'mape': mean_absolute_percentage_error}
+            dnn_results = {k: metric(test_split.y, dnn.predict(X_test)) for k, metric in metrics.items()}
+            dnn_results['fold'] = fold
+            dnn_results['features'] = features
+            pd.DataFrame([dnn_results])
+            results.append(pd.DataFrame([dnn_results]))
 
-# Print and save final results
-results = pd.concat(results, axis=0)
-print(f"Saving final results")
-print(results)
-results.to_csv(f"./results/cross_validation_results.txt", index=False)
+            # Save all intermediate results
+            print(f"Saving intermediate results results:")
+            intermediate_results = pd.concat(results, axis=0)
+            intermediate_results.to_csv(f"./results/partial_results{len(results)}.txt", index=False)
+
+    # Print and save final results
+    results = pd.concat(results, axis=0)
+    print(f"Saving final results")
+    print(results)
+    results.to_csv(f"./results/cross_validation_results.txt", index=False)
--- train_dnn_model.py
+++ train_dnn_model.py
@@ -10,16 +10,30 @@ def create_dnn(fgp_cols, binary_cols):
     return SkDnn(use_col_indices=fgp_cols, binary_col_indices=binary_cols, transform_output=True)
 
 
-def tune_and_fit(alvadesc_data, param_search_config):
+def tune_and_fit(alvadesc_data, param_search_config, features):
+    """
+    features: should be one of "fingerprints", "descriptors" or "all"
+    """
     print(f"Starting tune_and_fit with data with dim ({alvadesc_data.X.shape[0]},{alvadesc_data.X.shape[1]})")
     print("Preprocessing...")
-    preprocessor = FgpPreprocessor(
-        storage=param_search_config.storage,
-        study_prefix=f'preproc-{param_search_config.study_prefix}',
-        fgp_cols=alvadesc_data.fgp_cols,
-        n_trials=param_search_config.n_trials,
-        search_cv=param_search_config.param_search_cv
-    )
+    preprocessor = None
+    if features == "fingerprints":
+        print("Training fingerprints")
+        preprocessor = FgpPreprocessor(
+            storage=param_search_config.storage,
+            study_prefix=f'preproc-{param_search_config.study_prefix}',
+            fgp_cols=alvadesc_data.fgp_cols,
+            n_trials=param_search_config.n_trials,
+            search_cv=param_search_config.param_search_cv
+        )
+
+    elif features == "descriptors":
+        pass # raise NotImplemented("descriptors not implemented")
+    elif features == "all":
+        pass # raise NotImplemented("all not implemented")
+    else:
+        pass # raise ValueError('features: should be one of "fingerprints", "descriptors" or "all"')
+
     X_train = preprocessor.fit_transform(alvadesc_data.X, alvadesc_data.y)
 
     print("Creating DNN")
