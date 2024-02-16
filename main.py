a8e1aadff65ff42a43d30020d0f429f4a6bdd713
grfone <126663988+grfone@users.noreply.github.com>
2024-02-16 16:38:23 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 import pickle
 from sklearn.metrics import mean_absolute_error
@@ -6,7 +5,8 @@ from sklearn.metrics import median_absolute_error
 from sklearn.metrics import mean_absolute_percentage_error
 from sklearn.model_selection import StratifiedKFold
 
-from data import AlvadescDataset
+from utils.data_loading import get_my_data
+from utils.stratification import stratify_y
 from train_dnn_model import tune_and_fit
 
 from sklearn.model_selection import RepeatedKFold
@@ -14,39 +14,31 @@ from collections import namedtuple
 
 # Parameters
 features_list = ["fingerprints", "descriptors", "all"]
-SMOKE = False
+SMOKE = True
 #################
 
 if SMOKE:
+    print("Running smoke test...")
     amount_of_data = 2000
     number_of_folds = 2
     number_of_trials = 1
     param_search_folds = 2
     database = "sqlite:///./results/smokeDatabaseYouCanDeleteMe.db"
 else:
-    amount_of_data = "All"  # default all        minimum 2000
-    number_of_folds = 5  # default 10           minimum 2
-    number_of_trials = 100  # default 100         minimum 1
-    param_search_folds = 5  # default 5         minimum 2
+    amount_of_data = "All"
+    number_of_folds = 5
+    number_of_trials = 100
+    param_search_folds = 5
     database = "sqlite:///./results/cv.db"
 
-
-def stratify_y(y, n_strats=6):
-    ps = np.linspace(0, 1, n_strats)
-    # make sure last is 1, to avoid rounding issues
-    ps[-1] = 1
-    quantiles = np.quantile(y, ps)
-    cuts = pd.cut(y, quantiles, include_lowest=True)
-    codes, _ = cuts.factorize()
-    return codes
 
 if __name__=="__main__":
     # Load data
     print("Loading data")
-    data = AlvadescDataset("rt_data")
+    X, y, desc_cols, fgp_cols = get_my_data(common_cols=['unique_id', 'correct_ccs_avg'])
     if amount_of_data != "All":
-        print("*************** NOT USING ALL DATA **********************")
-        data = data[:amount_of_data]
+        X = X[:amount_of_data]
+        y = y[:amount_of_data]
 
 
     results = []
@@ -61,15 +53,17 @@ if __name__=="__main__":
         )
         cross_validation = StratifiedKFold(n_splits=number_of_folds, shuffle=True, random_state=42)
 
-        for fold, (train_index, test_index) in enumerate(cross_validation.split(data.X, stratify_y(data.y))):
+        for fold, (train_index, test_index) in enumerate(cross_validation.split(X, stratify_y(y))):
             param_search_config = param_search_config._replace(
                 study_prefix=base_prefix + f"-fold-{fold}"
             )
-            train_split = data[train_index]
-            test_split = data[test_index]
+            train_split_X = X[train_index]  # Unused
+            train_split_y = y[train_index]  # Unused
+            test_split_X = X[test_index]
+            test_split_y = y[test_index]
 
             preprocessor, dnn = (
-                tune_and_fit(data, param_search_config=param_search_config, features=features)
+                tune_and_fit(X, y, desc_cols, fgp_cols, param_search_config=param_search_config, features=features)
             )
 
             print("Saving preprocessor and DNN")
@@ -78,16 +72,16 @@ if __name__=="__main__":
             with open(f"./results/dnn-{features}-{fold}.pkl", "wb") as f:
                 pickle.dump(dnn, f)
 
-            X_test = preprocessor.transform(test_split.X)
+            X_test = preprocessor.transform(test_split_X)
             metrics = {'mae': mean_absolute_error, 'medae': median_absolute_error, 'mape': mean_absolute_percentage_error}
-            dnn_results = {k: metric(test_split.y, dnn.predict(X_test)) for k, metric in metrics.items()}
+            dnn_results = {k: metric(test_split_y, dnn.predict(X_test)) for k, metric in metrics.items()}
             dnn_results['fold'] = fold
             dnn_results['features'] = features
             pd.DataFrame([dnn_results])
             results.append(pd.DataFrame([dnn_results]))
 
             # Save all intermediate results
-            print(f"Saving intermediate results results:")
+            print(f"Saving intermediate results:")
             intermediate_results = pd.concat(results, axis=0)
             intermediate_results.to_csv(f"./results/partial_results{len(results)}.txt", index=False)
 
@@ -96,3 +90,4 @@ if __name__=="__main__":
     print(f"Saving final results")
     print(results)
     results.to_csv(f"./results/cross_validation_results.txt", index=False)
+
--- train_dnn_model.py
+++ train_dnn_model.py
@@ -10,34 +10,34 @@ def create_dnn(fgp_cols, binary_cols):
     return SkDnn(use_col_indices=fgp_cols, binary_col_indices=binary_cols, transform_output=True)
 
 
-def tune_and_fit(alvadesc_data, param_search_config, features):
+def tune_and_fit(X, y, desc_cols, fgp_cols, param_search_config, features):
     """
     features: should be one of "fingerprints", "descriptors" or "all"
     """
-    print(f"Starting tune_and_fit with data with dim ({alvadesc_data.X.shape[0]},{alvadesc_data.X.shape[1]})")
+    print(f"Starting tune_and_fit with data with dim ({X.shape[0]},{X.shape[1]})")
     print("Preprocessing...")
     if features == "fingerprints":
         print("Training fingerprints")
         preprocessor = FgpPreprocessor(
-            fgp_cols=alvadesc_data.fgp_cols
+            fgp_cols=fgp_cols
         )
 
     elif features == "descriptors":
         print("Training with descriptors")
         preprocessor = DescriptorsPreprocessor(
-            desc_cols=alvadesc_data.desc_cols,
-            adduct_cols=alvadesc_data.fgp_cols[-3:]
+            desc_cols=desc_cols,
+            adduct_cols=fgp_cols[-3:]
         )
     elif features == "all":
         print("Training with descriptors + fingerprints")
         preprocessor = Preprocessor(
-            desc_cols=alvadesc_data.desc_cols,
-            fgp_cols=alvadesc_data.fgp_cols
+            desc_cols=desc_cols,
+            fgp_cols=fgp_cols
         )
     else:
         raise ValueError('features: should be one of "fingerprints", "descriptors" or "all"')
 
-    X_train = preprocessor.fit_transform(alvadesc_data.X, alvadesc_data.y)
+    X_train = preprocessor.fit_transform(X, y)
 
     print("Creating DNN")
     all_cols = np.arange(X_train.shape[1])
@@ -49,7 +49,7 @@ def tune_and_fit(alvadesc_data, param_search_config, features):
     study = create_study("dnn", param_search_config.study_prefix, param_search_config.storage)
     best_params = param_search(
         dnn,
-        X_train, alvadesc_data.y,
+        X_train, y,
         cv=param_search_config.param_search_cv,
         study=study,
         n_trials=param_search_config.n_trials,
@@ -57,7 +57,7 @@ def tune_and_fit(alvadesc_data, param_search_config, features):
     )
     print("Training")
     dnn.set_params(**best_params)
-    dnn.fit(X_train, alvadesc_data.y)
+    dnn.fit(X_train, y)
 
     return preprocessor, dnn
 
